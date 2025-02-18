3e3cd107fccc84de6cb712ca479b35ba1410e176
evilel <encoru@rambler.ru>
2025-02-18 22:55:11 +0300

    feat:added vue config errorHandler

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import vuetify from "./shared/plugins/vuetify";
 import router from "./app/router";
 import App from "./app";
 import { sentryInit } from "@/shared/api/sentryInit";
-
+import { globalErrorHandler } from "@/shared/api/errorHandler";
 const app = createApp(App);
+
+app.config.errorHandler = globalErrorHandler;
 
 sentryInit(app, router);
 app.use(vuetify).use(router);
---
+++ src/shared/api/errorHandler.ts
@@ -0,0 +1,12 @@
+import { ComponentPublicInstance } from "vue";
+
+export function globalErrorHandler(
+  err: unknown,
+  instance: ComponentPublicInstance | null,
+  info: string
+) {
+  if (err instanceof Error) {
+    console.log("error", err.message);
+  }
+  console.log("globalErrorHandler", err, instance, info);
+}
