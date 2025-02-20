6ff37155d95d1dba93be1d2e41a43509db06bf44
evilel <encoru@rambler.ru>
2025-02-20 20:17:14 +0300

    fix:change style on style.modules

--- src/pages/Task/ui/TaskManager/TaskManager.vue
+++ src/pages/Task/ui/TaskManager/TaskManager.vue
@@ -5,6 +5,7 @@ import Dialog from "@/features/dialog";
 import useTask from "@/entities/task/api/useTask";
 import TaskPayload from "../TaskPayload";
 import { useTaskManagerDialog } from "@/pages/Task/model";
+import style from "./TaskManager.module.css";
 
 const {
   inProgressTasks,
@@ -18,57 +19,57 @@ const { isDialog, openDialog } = useTaskManagerDialog();
 </script>
 
 <template>
-  <div class="task-manager">
+  <div :class="style['task-manager']">
     <v-btn
-      class="task-manager__add"
+      :class="style['task-manager__add']"
       prepend-icon="mdi-plus-circle"
       variant="text"
       @click.stop="openDialog"
     >
       Задача
     </v-btn>
-    <Dialog
-      v-model:is-dialog="isDialog"
-      :options="{ title: 'Добавить задачу', maxWidth: 500 }"
-    >
-      <TaskPayload />
-    </Dialog>
-    <div class="task-manager__content">
-      <div class="task-manager__draggable">
+    <div :class="style['task-manager__content']">
+      <div :class="style['task-manager__draggable']">
         В процессе: {{ countInProgressTasks }}
         <draggable
           v-model="inProgressTasks"
-          class="task-manager__list-group"
+          :class="style['task-manager__list-group']"
           item-key="id"
           group="task"
           @change="moveTask"
         >
           >
           <template #item="{ element }">
-            <div class="list-group-item">
+            <div :class="style['list-group-item']">
               {{ element.title }}
             </div>
           </template>
         </draggable>
       </div>
-      <div class="task-manager__draggable">
+      <div :class="style['task-manager__draggable']">
         Выполнены: {{ countCompletedTasks }} / {{ countTask }}
         <draggable
           v-model="completedTasks"
-          class="task-manager__list-group"
+          :class="style['task-manager__list-group']"
           item-key="id"
           group="task"
           @change="moveTask"
         >
           <template #item="{ element }">
-            <div class="list-group-item">{{ element.title }}</div>
+            <div :class="style['list-group-item']">{{ element.title }}</div>
           </template>
         </draggable>
       </div>
     </div>
   </div>
+  <Dialog
+    v-model:is-dialog="isDialog"
+    :options="{ title: 'Добавить задачу', maxWidth: 500 }"
+  >
+    <TaskPayload />
+  </Dialog>
 </template>
 
 <style scoped>
-@import "./TaskManager.module.css";
+/* @import "./TaskManager.css"; */
 </style>
