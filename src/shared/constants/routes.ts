f72e605df4ff666f10a3e8141c194f6fdc1d6e5e
Michael Andreev <enc0ru@rambler.ru>
2025-03-20 19:03:10 +0300

    fix: export

--- src/shared/constants/routes.ts
+++ src/shared/constants/routes.ts
@@ -1,9 +1,7 @@
-enum RoutesName {
+export enum RoutesName {
   HOME = 'home',
   LOGIN = 'login',
   TASK = 'task',
   REGISTRATION = 'registration',
   PASSWORD_RECOVERY = 'passwordRecovery',
 }
-
-export default { RoutesName }
